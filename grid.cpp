18bea07cd59f7707951751dc27bdd03f57ee7e8d
anicanor <31227133+anicanor@users.noreply.github.com>
2018-04-30 21:54:52 -0700

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -1,37 +1,47 @@
 //grid.cpp
 //Aaron Nicanor
 //anicanor
+
 #include<iostream>
 #include "grid.h"
 
 using namespace std;
 
+// The number of Rows and Columns
+const int ROWS = 24;
+const int COLS = 60;
+
 //Prints the grid
 void Grid::print(){
-
-  for(int i = 0; i <24; i++)  
-    for(int o = 0; o < 60; o++)
-      cout << m_grid[o][i];
-    cout << endl;
   
+    for(unsigned int i = 0; i < grid.size(); i++){
+      
+        for(unsigned int o = 0; o < grid[0].size(); o++)
+          cout<<grid[i][o];
+        
+        cout << "\n"; //endl;
+    }
 }
 
 void Grid::set(int x,int y, char input){
   
-  // If the input is trying to put a character outside of the grid, this will stop it
-  if(x>=60 || y>= 24)
-    return;
-  else
-    m_grid[x][y]=input;
-
+    // If the input is trying to put a character outside of the grid, this will stop it
+    if(x < COLS && y >= 0 && y < ROWS && x >= 0)
+        grid[y][x] = input;
 }
 
 //Constructor
 Grid::Grid(){
-
-  for(int i = 0; i < 60; i++)
-    for(int o = 0; o <24; o++)
-      m_grid[i][o] = ' ';
-    
+  
+  grid.resize(ROWS);
+      for(unsigned int i = 0; i < grid.size(); i++)
+        grid[i].resize(COLS);
+  
+      for(unsigned int i = 0; i < grid.size(); i++){
+        for(unsigned int o = 0; o < grid[0].size(); o++){
+          
+            grid[i][o] = ' ';
+        }
+      }
 }
 
