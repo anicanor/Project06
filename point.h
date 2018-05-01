b417230d195013ec0643b1f287c95fdfe765621a
anicanor <31227133+anicanor@users.noreply.github.com>
2018-04-30 21:58:50 -0700

    Update point.h

--- point.h
+++ point.h
@@ -5,20 +5,21 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include<iostream>
+#include<vector>
+
 #include "shape.h"
 #include "grid.h"
-
-#include<iostream>
 
 using namespace std;
 
 class Point:public Shape{
 
   public:
-    void draw(Grid &grid);
-    Point(int x, int y, char input); 
+    void draw(Grid &);
+    Point(int , int , char ); 
   
-  protected:
+  private:
     char m_input;
     
 };
