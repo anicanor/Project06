7bc8421ac3e1598bfbd14d1462037c9e3109ce6a
anicanor <31227133+anicanor@users.noreply.github.com>
2018-04-30 22:04:53 -0700

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -2,19 +2,18 @@
 //Aaron Nicanor
 //anicanor
 
-#include "triangle.h"
-#include "shape.h"
-#include "grid.h"
-
 #include<iostream>
+#include<vector>
+
+#include "triangle.h"
 
 using namespace std;
 
 //Draws out the Triangle
 void Triangle :: draw(Grid &grid){
 
-  grid.set(m_x+2,m_y,'+');
-  grid.set(m_x+1,m_y+1, '+');
+  grid.set(m_x + 2, m_y,'+');
+  grid.set(m_x + 1, m_y + 1, '+');
   grid.set(m_x + 3, m_y + 1, '+');
   grid.set(m_x , m_y + 2, '+');
   grid.set(m_x + 1, m_y + 2, '+');
